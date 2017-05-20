546b81a4a4a6fb513be2fdd1103672d303e3e6e5
felipesilvadv <lfsilva1@uc.cl>
2017-05-19 20:23:10 -0400

    T5 mate-discreta

--- ALN/Tareas/T4/tests.m
+++ ALN/Tareas/T4/tests.m
@@ -1,7 +1,9 @@
+Conjugate_Gradient
+Conjugate_Residual
 [xR, flagR, relresR, iterR, resvecR] = CR(A, b, tol, maxit,x0);
 [x, flag, relres, iter, resvec] = CG(A, b, tol, maxit,x0);
 condicionamiento = cond(A,2)
-A(n,n) = 1000;
+A(n,n) = 10000000;
 condicionamiento = cond(A,2)
 [xR1, flagR1, relresR1, iterR1, resvecR1] = CR(A, b, tol, maxit,x0);
 [x1, flag1, relres1, iter1, resvec1] = CG(A, b, tol, maxit,x0);
